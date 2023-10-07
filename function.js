fd06fa5971a970b74a46b26d246b9fb8c84943e6
Palienko Mihai <palienko@advantum.pro>
2023-10-07 13:51:34 +0300

    functions

---
+++ function.js
@@ -0,0 +1,38 @@
+// const makeCalculation = (operator, text, secondText = 'Введите второе число') => {
+//     const firstNum = prompt(text ? text : 'Введите первое число');
+//     const secondNum = prompt(secondText);
+
+//     let result;
+//     if (operator === '+') {
+//         result = +firstNum + +secondNum;
+//     }
+
+//     if (operator === '-') {
+//         result = +firstNum - +secondNum;
+//     }
+
+//     return result
+
+// }
+
+// const result =  makeCalculation('-', 'Веди пожаааалуйста первое число')
+
+// console.log(result)
+// alert(result)
+
+
+
+let a = 'a'
+
+const foo = () => {
+
+    let a = 'b'
+
+    console.log(a)
+
+}
+
+console.log(a)
+
+
+foo()
