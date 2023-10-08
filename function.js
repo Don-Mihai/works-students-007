2bacdafa89ede68564178d4544ea72b89d2701af
AlexeyOkhotnikov <alexeyps84@gmail.com>
2023-10-08 10:59:12 +0300

    functionsJs

---
+++ function.js
@@ -0,0 +1,39 @@
+// const makeCalculator = () => {
+
+// }
+
+// let example = 'example';
+
+// const foo = () => {
+
+//     let example = 'localExample'
+//     second = 'test'
+//     const boo = () => {
+//         console.log("boo")
+
+//         example = 'automaticTest'
+//         const faa = () => {
+//             console.log("faa")
+//         };
+
+//         faa()
+//     };
+
+//     boo()
+// };
+
+// foo()
+
+// console.log(example)
+
+let a = "a"
+
+const foo = () => {
+    let a = 'b'
+
+    console.log(a)
+}
+
+foo()
+
+
