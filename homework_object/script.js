26c60d19e108d830f299da21d215dd1ef9fe1576
ruslan <rnagumanov57@gmail.com>
2023-10-13 14:28:34 +0300

    JS_object

---
+++ homework_object/script.js
@@ -0,0 +1,48 @@
+// 1я задача
+// const user = {
+//   name: "John",
+//   surname: "Smith",
+//   name: "Pete",
+// };
+// console.log(user);
+// delete user.name;
+// 2я задача
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// let sum = Object.values(salaries).reduce((a, b) => a + b, 0);
+// console.log(`sum is a ${sum}`);
+// 3я задача
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+// function multiplyNumeric(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] == "number") {
+//       obj[key] *= 2;
+//     }
+//   }
+// }
+// multiplyNumeric(menu);
+// console.log(menu);
+// 4я задача
+// let schedule = {};
+
+// console.log(isEmpty(schedule));
+
+// schedule["8:30"] = "get up";
+
+// console.log(isEmpty(schedule));
+
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// console.log(schedule);
