da81009af86bc7d9a2077fc9aeefdd93be380cc6
ruslan <rnagumanov57@gmail.com>
2023-10-14 13:52:34 +0300

    [cloneObject]

---
+++ clone_object/array.js
@@ -0,0 +1,55 @@
+// const user = {
+//   name: "jopa",
+//   age: 10200,
+// };
+
+// const numbers = ["a", "b", "c", "d"];
+
+// for (let i = 0; i < numbers.length; ++i) {
+//   console.log(`${i + 1} , ${numbers[i]}`);
+// }
+
+// console.log(numbers);
+// --------------- 1method
+// const numbers = ["a", "b", "c", "d"];
+// numbers.forEach((item, index) => {
+//   console.log(item, index + 1);
+// });
+// console.log(numbers);
+// --------------- 2method
+// const numbers = [1, 2, 3, 4];
+// const newArr = numbers.map((item) => {
+//   return item * 30;
+// });
+// console.log(newArr);
+// console.log(numbers);
+// --------------- 3method
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const filteredArr = numbers.filter((item) => (item >= 4 ? true : false));
+// console.log(filteredArr);
+
+// const products = [
+//   {
+//     name: "apple",
+//     price: "$1",
+//     category: "fruits",
+//   },
+//   {
+//     name: "banana",
+//     price: "$2",
+//     category: "fruits",
+//   },
+//   {
+//     name: "orange",
+//     price: "$3",
+//     category: "fruits",
+//   },
+//   {
+//     name: "table",
+//   },
+// ];
+
+// const filteredArr = products.filter((product) =>
+//   product.category === "fruits" ? true : false
+// );
+// console.log(filteredArr);
---
+++ clone_object/clone.js
@@ -0,0 +1,60 @@
+// const user = {
+//     name: "John",
+//     age: 30,
+//     gender: "male",
+//     interests:'Музыка, MMA',
+// }
+
+// const saller = user
+
+// saller.name = 'Курбан'
+
+// user.gender = 'binary'
+
+// console.log(saller)
+
+// -------------------------- clone object
+
+// const user = {
+//   name: "John",
+//   age: 30,
+//   gender: "male",
+//   interests: "Музыка, MMA",
+//   role: {
+//     isAdmin: true,
+//   },
+// };
+
+// const role = {
+//   name: "sosi",
+//   isAdmin: false,
+//   isManager: true,
+// };
+
+// const person = { ...role, ...user };
+// console.log(person);
+
+// const person = { ...user };
+
+// person.name = "sos";
+
+// console.log({
+//   ...user.role,
+// });
+
+// let zorro = 'конь'
+
+// let superman = zorro
+
+// superman = 'трусы красные'
+
+// // zorro = конь
+// // superman = трусы красные
+// console.log(zorro)
+//-------------------- optional chain
+// const user = {
+//   fio: "John",
+//   //   roles: ["admin", "gay"],
+// };
+
+// console.log(user?.roles?.length);
