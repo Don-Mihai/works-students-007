82615a5c102707508fbc31f0181ad2f8544e7b40
Palienko Mihai <palienko@advantum.pro>
2023-10-15 13:09:17 +0300

    fetch

--- array.js
+++ array.js
@@ -25,4 +25,53 @@
 
 // console.log(newArr)
 
-// ------------3--------------------
+// ------------3--- filter-----------------
+// const numbers = [1, 2, 3, 4, 5, 6]
+
+// const filteredArr = numbers.filter((item) => item >= 4 ? true : false)
+
+// console.log(filteredArr)
+
+// const products = [
+//     {
+//         name: 'apple',
+//         price: '$1',
+//         category: 'fruits',
+//     },
+//     {
+//         name: 'orange',
+//         price: '$2',
+//         category: 'fruits',
+//     },
+//     {
+//         name: 'banana',
+//         price: '$3',
+//         category: 'fruits',
+//     },
+//     {
+//         name: 'table',
+//     }
+// ];
+
+// const filteredArr = products.filter((product) => product.category === 'fruits' ? true : false )
+
+// console.log(filteredArr)
+// -----------4-----reduce----------------------
+
+// const arr = [1 , 2, 3, 4, 5]
+
+// const result =  arr.reduce((result, item) => {
+//     return result + item
+// }, 0)
+
+// console.log(result)
+
+// -----------5----push pop shift unshift--------------------
+
+// const arr = [1, 2, 3]
+
+// arr.push(4)
+
+// arr.pop()
+
+// console.log(arr)
