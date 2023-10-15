53cbd816f735dec5f22ad651da6eafdd870446d6
ruslan <rnagumanov57@gmail.com>
2023-10-15 13:55:16 +0300

    [addchanges]array

--- clone_object/array.js
+++ clone_object/array.js
@@ -78,3 +78,57 @@
 // arr.pop();
 
 // console.log(arr);
+/* function likes(names) {
+  const n = names.length;
+  switch (n) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${n - 2} others like this`;
+  }
+}
+console.log(likes([]));
+console.log(likes(["Peter"]));
+console.log(likes(["Jacob", "Alex"]));
+console.log(likes(["Max", "John", "Mark"]));
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); */
+
+// function likes(names) {
+//   switch (names.length) {
+//     case 0:
+//       return "no one likes this";
+//     case 1:
+//       return names[0] + " likes this";
+//     case 2:
+//       return names[0] + " and " + names[1] + " like this";
+//     case 3:
+//       return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+//     default:
+//       return (
+//         names[0] +
+//         ", " +
+//         names[1] +
+//         " and " +
+//         (names.length - 2) +
+//         " others like this"
+//       );
+//   }
+// }
+// function likes(names) {
+//   const n = names.length;
+//   return n === 0
+//     ? "no one likes this"
+//     : n === 1
+//     ? `${names[0]} likes this`
+//     : n === 2
+//     ? `${names[0]} and ${names[1]} like this`
+//     : n === 3
+//     ? `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     : `${names[0]}, ${names[1]} and ${n - 2} others like this`;
+// }
