97da4d26b6c7fead90e3a2dfd8354c1bd1a33431
AlexeyOkhotnikov <alexeyps84@gmail.com>
2023-10-21 11:06:31 +0300

    [fetch_dz]

---
+++ fetchDz.js
@@ -0,0 +1,52 @@
+async function getUsersData() {
+    try {
+        // Пункт 1: Получить данные всех пользователей
+        const response = await fetch('https://reqres.in/api/users?per_page=12');
+        const data = await response.json();
+        const users = data.data;
+        console.log(data)
+
+        console.log('-----------');
+        console.log('Вывести фамилии всех пользователей в цикле:');
+        console.log('-----------');
+
+        // Пункт 2: Вывести фамилии всех пользователей в цикле
+        for (const user of users) {
+            console.log(user.last_name);
+        }
+
+        console.log('-----------');
+        console.log('Вывести все данные пользователей, фамилия которых начинается с "F":');
+        console.log('-----------');
+
+        // Пункт 3: Вывести все данные пользователей, фамилия которых начинается с "F"
+        for (const user of users) {
+            if (user.last_name.startsWith('F')) {
+                console.log(user);
+            }
+        }
+
+        console.log('-----------');
+        console.log('Вывести имена и фамилии всех пользователей с использованием метода reduce');
+        console.log('-----------');
+
+        // Пункт 4: Вывести имена и фамилии всех пользователей с использованием метода reduce
+        const allUserData = users.reduce((acc, user) => {
+            return acc + user.first_name + ' ' + user.last_name + ', ';
+        }, 'Наша база содержит данные следующих пользователей: ');
+
+        console.log(allUserData.slice(0, -2)); // Убираем последнюю запятую и пробел
+
+        console.log('-----------');
+        console.log('Вывести названия всех ключей в объекте пользователя:');
+        console.log('-----------');
+
+        // Пункт 5: Вывести названия всех ключей в объекте пользователя
+        const userKeys = Object.keys(users[0]);
+        console.log(userKeys);
+    } catch (error) {
+        console.error('Произошла ошибка при получении данных:', error);
+    }
+}
+
+getUsersData();
